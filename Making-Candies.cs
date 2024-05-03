4ff44f6e4fb1b8991547b3b076e9457ad6c7d679
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-05-03 13:00:51 +0900

    Update Making-Candies.cs

--- Making-Candies.cs
+++ Making-Candies.cs
@@ -1,1 +1,34 @@
-C#
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'minimumPasses' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. LONG_INTEGER m
+     *  2. LONG_INTEGER w
+     *  3. LONG_INTEGER p
+     *  4. LONG_INTEGER n
+     */
+
+    public static long minimumPasses(long m, long w, long p, long n)
+    {
+
+    }
+
+}
