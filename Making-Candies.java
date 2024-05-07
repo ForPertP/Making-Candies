b0c36cf69fad13d074a6d1901418695576d509ec
ForPertP ! <50106601+ForPertP@users.noreply.github.com>
2024-05-07 22:37:36 +0900

    Update Making-Candies.java

--- Making-Candies.java
+++ Making-Candies.java
@@ -1,1 +1,35 @@
-Java
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'minimumPasses' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. LONG_INTEGER m
+     *  2. LONG_INTEGER w
+     *  3. LONG_INTEGER p
+     *  4. LONG_INTEGER n
+     */
+
+    public static long minimumPasses(long m, long w, long p, long n) {
+        long passes = 0;
+        long candy = 0;
+        long step = 0;
+        long minTotalPasses = Long.MAX_VALUE;
+
+        return minTotalPasses;
+    }
+}
+
